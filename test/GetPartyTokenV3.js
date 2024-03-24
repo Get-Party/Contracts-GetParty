8813babd3253944cabb0eae6aacef4df70155390
bonino97 <juanbonino97@gmail.com>
2024-03-24 17:23:20 +0100

    Add more test cases

--- test/GetPartyTokenV3.js
+++ test/GetPartyTokenV3.js
@@ -24,61 +24,92 @@ describe("GetPartyTokenV3", function () {
         await getPartyTokenV3.enableTrading();
     });
 
-    it("Should return the total supply", async function () {
-        expect(await getPartyTokenV3.totalSupply()).to.equal(totalSupply);
+    describe('Token properties', function () {
+        it("Should return the total supply", async function () {
+            expect(await getPartyTokenV3.totalSupply()).to.equal(totalSupply);
+        });
+
+        it("Should return the balance of the owner", async function () {
+            expect(await getPartyTokenV3.balanceOf(owner.address)).to.equal(totalSupply);
+        });
+
+        it('Should set the right owner', async function () {
+            expect(await getPartyTokenV3.owner()).to.equal(owner.address);
+        });
+
+        it('Should assign the total supply of tokens to the owner', async function () {
+            const ownerBalance = await getPartyTokenV3.balanceOf(owner.address);
+            expect(await getPartyTokenV3.totalSupply()).to.equal(ownerBalance);
+        });
     });
 
-    it("Should return the balance of the owner", async function () {
-        expect(await getPartyTokenV3.balanceOf(owner.address)).to.equal(totalSupply);
+    describe('Token transfers', function () {
+        it('Should transfer tokens between accounts', async function () {
+            await getPartyTokenV3.transfer(addr1.address, 50);
+            const addr1Balance = await getPartyTokenV3.balanceOf(addr1.address);
+            expect(addr1Balance).to.equal(50);
+
+            await getPartyTokenV3.connect(addr1).transfer(addr2.address, 50);
+            const addr2Balance = await getPartyTokenV3.balanceOf(addr2.address);
+            expect(addr2Balance).to.equal(50);
+        });
+
+        it('Should fail if sender doesn’t have enough tokens', async function () {
+            const initialOwnerBalance = await getPartyTokenV3.balanceOf(owner.address);
+            await expect(
+                getPartyTokenV3.connect(addr1).transfer(owner.address, 1)
+            ).to.be.revertedWith('ERC20: transfer amount exceeds balance');
+
+            expect(await getPartyTokenV3.balanceOf(owner.address)).to.equal(
+                initialOwnerBalance
+            );
+        });
+
+        it('Should update balances after transfers', async function () {
+            const initialOwnerBalance = await getPartyTokenV3.balanceOf(owner.address);
+
+            await getPartyTokenV3.transfer(addr1.address, 100);
+            await getPartyTokenV3.transfer(addr2.address, 50);
+
+            const finalOwnerBalance = await getPartyTokenV3.balanceOf(owner.address);
+            expect(finalOwnerBalance).to.equal(initialOwnerBalance - BigInt(150));
+
+            const addr1Balance = await getPartyTokenV3.balanceOf(addr1.address);
+            expect(addr1Balance).to.equal(BigInt(100));
+
+            const addr2Balance = await getPartyTokenV3.balanceOf(addr2.address);
+            expect(addr2Balance).to.equal(BigInt(50));
+        });
     });
 
+    describe('Trading', function () {
+        it('Should return error when trading is enabled and try to enable again', async function () {
+            await expect(getPartyTokenV3.enableTrading()).to.be.revertedWith('Trading already enabled.');
+        });
 
-    it('Should set the right owner', async function () {
-        console.log('owner.address: ', owner.address)
-        expect(await getPartyTokenV3.owner()).to.equal(owner.address);
+        it('Should allow trading after it is enabled', async function () {
+            await getPartyTokenV3.transfer(addr1.address, 50);
+            expect(await getPartyTokenV3.balanceOf(addr1.address)).to.equal(50);
+        });
     });
 
-    it('Should assign the total supply of tokens to the owner', async function () {
-        const ownerBalance = await getPartyTokenV3.balanceOf(owner.address);
-        expect(await getPartyTokenV3.totalSupply()).to.equal(ownerBalance);
-    });
+    describe('Treasury Wallet', function () {
+        it('Should not allow to set the treasury wallet to the zero address', async function () {
+            await expect(
+                getPartyTokenV3.changeTreasuryWallet(ethers.ZeroAddress)
+            ).to.be.revertedWith('Marketing wallet cannot be the zero address');
+        });
 
-    it('Should transfer tokens between accounts', async function () {
-        await getPartyTokenV3.transfer(addr1.address, 50);
-        const addr1Balance = await getPartyTokenV3.balanceOf(addr1.address);
-        expect(addr1Balance).to.equal(50);
+        it('Should not allow to set the treasury wallet to a contract address', async function () {
+            const contractAddress = await getPartyTokenV3.getAddress();
+            await expect(
+                getPartyTokenV3.changeTreasuryWallet(contractAddress)
+            ).to.be.revertedWith('Marketing wallet cannot be a contract');
+        });
 
-        await getPartyTokenV3.connect(addr1).transfer(addr2.address, 50);
-        const addr2Balance = await getPartyTokenV3.balanceOf(addr2.address);
-        expect(addr2Balance).to.equal(50);
-    });
-
-    it('Should fail if sender doesn’t have enough tokens', async function () {
-        const initialOwnerBalance = await getPartyTokenV3.balanceOf(owner.address);
-        console.log('initialOwnerBalance: ', initialOwnerBalance)
-
-        await expect(
-            getPartyTokenV3.connect(addr1).transfer(owner.address, 1)
-        ).to.be.revertedWith('ERC20: transfer amount exceeds balance');
-
-        expect(await getPartyTokenV3.balanceOf(owner.address)).to.equal(
-            initialOwnerBalance
-        );
-    });
-
-    it('Should update balances after transfers', async function () {
-        const initialOwnerBalance = await getPartyTokenV3.balanceOf(owner.address);
-
-        await getPartyTokenV3.transfer(addr1.address, 100);
-        await getPartyTokenV3.transfer(addr2.address, 50);
-
-        const finalOwnerBalance = await getPartyTokenV3.balanceOf(owner.address);
-        expect(finalOwnerBalance).to.equal(initialOwnerBalance - BigInt(150));
-
-        const addr1Balance = await getPartyTokenV3.balanceOf(addr1.address);
-        expect(addr1Balance).to.equal(BigInt(100));
-
-        const addr2Balance = await getPartyTokenV3.balanceOf(addr2.address);
-        expect(addr2Balance).to.equal(BigInt(50));
+        it('Should allow to change the treasury wallet', async function () {
+            await getPartyTokenV3.changeTreasuryWallet(addr1.address);
+            expect(await getPartyTokenV3.treasuryWallet()).to.equal(addr1.address);
+        });
     });
 });
